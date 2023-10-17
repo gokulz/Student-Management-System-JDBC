09db2a45346d73eb42277e71ec4ef711ae85d7ac
Gokul MS <gokulakannan877@gmail.com>
2023-10-17 18:25:36 +0530

    Added the CURD operations in the project
    Added the CRUD operations Create,Read,Update,Delete

--- src/DataSource.java
+++ src/DataSource.java
@@ -1,7 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 /**
  * this class will be used to connect with database
@@ -17,11 +14,22 @@ public class DataSource {
     public static final String COLUMN_STUDENT_GRADE = "student_grade";
     public static final String COLUMN_STUDENT_GENDER = "student_gender";
 
-   public static final String INSERT_STUDENT_DETAILS = "INSERT INTO " + TABLE_NAME + '(' + COLUMN_STUDENT_ID + ", " +
-           COLUMN_STUDENT_NAME + ", " + COLUMN_STUDENT_GRADE + ", " + COLUMN_STUDENT_GENDER + ") VALUES(?,?,?,?)";
+    public static final String INSERT_STUDENT_DETAILS = "INSERT INTO " + TABLE_NAME + '(' + COLUMN_STUDENT_ID + ", " +
+            COLUMN_STUDENT_NAME + ", " + COLUMN_STUDENT_GRADE + ", " + COLUMN_STUDENT_GENDER + ") VALUES(?,?,?,?)";
+
+    public static final String UPDATE_STUDENT_DETAILS = "UPDATE " + TABLE_NAME + " SET " + COLUMN_STUDENT_NAME + " =?," +
+            COLUMN_STUDENT_GRADE + " =?, " + COLUMN_STUDENT_GENDER + " =? " + " WHERE " + COLUMN_STUDENT_ID + " =?";
+
+    public static final String DELETE_STUDENT_DETAILS = "DELETE FROM " + TABLE_NAME + " WHERE " + COLUMN_STUDENT_ID + " =?";
+    public static final String VIEW_STUDENT_DETAILS = "SELECT * FROM " + TABLE_NAME;
+
+
 
     private Connection connection;
     private PreparedStatement insertStudentDetails;
+    private PreparedStatement updateStudentDetails;
+    private PreparedStatement deleteStudentDetails;
+    private PreparedStatement viewStudentDetails;
 
 
     /**
@@ -29,26 +37,34 @@ public class DataSource {
      * @return database connection
      */
     public boolean open(){
-         try{
+        try{
 
-             connection = DriverManager.getConnection(CONNECTION_STRING);
+            connection = DriverManager.getConnection(CONNECTION_STRING);
 
-             String createTableSQL = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " (" +
-                     COLUMN_STUDENT_ID + " INTEGER, " +
-                     COLUMN_STUDENT_NAME + " TEXT, " +
-                     COLUMN_STUDENT_GRADE + " TEXT, " +
-                     COLUMN_STUDENT_GENDER + " TEXT)";
+            String createTableSQL = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " (" +
+                    COLUMN_STUDENT_ID + " INTEGER, " +
+                    COLUMN_STUDENT_NAME + " TEXT, " +
+                    COLUMN_STUDENT_GRADE + " TEXT, " +
+                    COLUMN_STUDENT_GENDER + " TEXT)";
 
-             connection.createStatement().execute(createTableSQL);
+            connection.createStatement().execute(createTableSQL);
 
-             insertStudentDetails = connection.prepareStatement(INSERT_STUDENT_DETAILS);
-             return true;
-         } catch (SQLException e){
-             System.out.println("Couldn't connected to database " + e.getMessage());
-             e.printStackTrace();
-             return false;
-         }
+            return true;
+        } catch (SQLException e){
+            System.out.println("Couldn't connected to database " + e.getMessage());
+            e.printStackTrace();
+            return false;
+        }
     }
+
+    /**
+     * method to insert the student details into the database.
+     * @param student_id  get the student id
+     * @param student_name get the student name
+     * @param student_grade get the student grade
+     * @param student_gender get the student gender
+     * @throws SQLException catch the exception if it goes wrong
+     */
 
     public void insertStudentDetails(int student_id, String student_name,String student_grade, String student_gender) throws SQLException{
         insertStudentDetails = connection.prepareStatement(INSERT_STUDENT_DETAILS);
@@ -63,6 +79,64 @@ public class DataSource {
         } else{
             System.out.println("Data not inserted");
         }
+
+    }
+
+    /**
+     * update the details per given details
+     * @param student_id to get student id
+     * @param student_name to get student name
+     * @param student_grade to get student grade
+     * @param student_gender to get student gender
+     * @throws SQLException catch the exception if it goes wrong
+     */
+    public void updateStudentDetails(int student_id, String student_name, String student_grade, String student_gender) throws SQLException{
+        updateStudentDetails = connection.prepareStatement(UPDATE_STUDENT_DETAILS);
+        updateStudentDetails.setString(1,student_name);
+        updateStudentDetails.setString(2,student_grade);
+        updateStudentDetails.setString(3,student_gender);
+        updateStudentDetails.setInt(4,student_id);
+
+        int rowAffected = updateStudentDetails.executeUpdate();
+        if(rowAffected>0){
+            System.out.println("Data Updated Successfully into the database");
+        } else{
+            System.out.println("Data not updated");
+        }
+
+    }
+
+    /**
+     * delete the student details for given id
+     * @param student_id to get student id
+     * @throws SQLException catch the exception if it goes wrong
+     */
+    public void deleteStudentDetails(int student_id) throws SQLException{
+        deleteStudentDetails = connection.prepareStatement(DELETE_STUDENT_DETAILS);
+        deleteStudentDetails.setInt(1,student_id);
+
+        int rowAffected = deleteStudentDetails.executeUpdate();
+        if(rowAffected >0){
+            System.out.println("The data of " + student_id + " is successfully deleted ");
+        } else {
+            System.out.println("The data of " + student_id +  " is not deleted ");
+        }
+    }
+
+    public void viewStudentDetails() throws SQLException{
+        viewStudentDetails = connection.prepareStatement(VIEW_STUDENT_DETAILS);
+        ResultSet results = viewStudentDetails.executeQuery();
+
+        while (results.next()){
+            int studentId = results.getInt(COLUMN_STUDENT_ID);
+            String studentName = results.getString(COLUMN_STUDENT_NAME);
+            String studentGrade = results.getString(COLUMN_STUDENT_GRADE);
+            String studentGender = results.getString(COLUMN_STUDENT_GENDER);
+
+            System.out.println("Student Id: " +  studentId + " " + " Student Name: " + studentName +  " " + "Student Grade: " + studentGrade + " " +
+                    "Student Gender: " + studentGender);
+        }
+
 
     }
 
--- src/Main.java
+++ src/Main.java
@@ -8,25 +8,91 @@ public class Main {
         if(!dataSource.open()){
             System.out.println("couldn't open datasource");
         }
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the Student ID: ");
-        int id = sc.nextInt();
-        System.out.print("Enter The Student Name: ");
-        sc.nextLine();
-        String name = sc.nextLine();
-        System.out.print("Enter The Student grade: ");
-        String grade = sc.nextLine();
-        System.out.print("Enter The Student gender: ");
-        String gender = sc.nextLine();
 
-        try {
-            dataSource.insertStudentDetails(id,
-                    name,
-                    grade,
-                    gender);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            System.out.println();
+            System.out.println("1. Insert the student");
+            System.out.println("2. Update the student");
+            System.out.println("3. Delete the student");
+            System.out.println("4. View the student");
+            System.out.println("5. Exit the student");
+            System.out.println();
+
+            System.out.print("Enter the choice: ");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter the Student ID: ");
+                    int id = sc.nextInt();
+                    System.out.print("Enter The Student Name: ");
+                    sc.nextLine();
+                    String name = sc.nextLine();
+                    System.out.print("Enter The Student grade: ");
+                    String grade = sc.nextLine();
+                    System.out.print("Enter The Student gender: ");
+                    String gender = sc.nextLine();
+
+                    try {
+                        dataSource.insertStudentDetails(id,
+                                name,
+                                grade,
+                                gender);
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+                case 2:
+                    System.out.print("Enter the Student ID: ");
+                    id = sc.nextInt();
+                    System.out.print("Enter The Student Name: ");
+                    sc.nextLine();
+                    name = sc.nextLine();
+                    System.out.print("Enter The Student grade: ");
+                    grade = sc.nextLine();
+                    System.out.print("Enter The Student gender: ");
+                    gender = sc.nextLine();
+                    try{
+                        dataSource.updateStudentDetails(id,
+                                name,
+                                grade,
+                                gender);
+                    } catch (SQLException e) {
+                        System.out.println("Failed " + e.getMessage());
+                        e.printStackTrace();
+                    }
+                    break;
+
+                case 3:
+                    System.out.print("Enter the Student id to delete: ");
+                    id = sc.nextInt();
+                    try{
+                        dataSource.deleteStudentDetails(id);
+                    } catch (SQLException e){
+                        System.out.println("Failed " + e.getMessage());
+                        e.printStackTrace();
+                    } break;
+                case 4:
+                    try{
+                        dataSource.viewStudentDetails();
+                    } catch (SQLException e){
+                        System.out.println("Failed to load students " + e.getMessage());
+                        e.printStackTrace();
+                    } break;
+                case 5:
+                    System.out.println("Exit");
+                    System.out.println("Thank You");
+                    System.exit(0);
+                    sc.close();
+                    break;
+                default:
+                    System.out.println("Invalid Choice");
+
+            }
+
         }
+
 
 
     }
